42a3dca1779aa99bd67e5c7318c7adf60fe878f2
octopusnz <23374251+octopusnz@users.noreply.github.com>
2020-12-07 11:32:53 +1300

    Update build_msvc.bat

--- build_msvc.bat
+++ build_msvc.bat
@@ -2,7 +2,6 @@
 rem launch this from msvs-enabled console
 
 set CFLAGS=/std:c17 /O2 /FC /Zi /W4 /WX /wd4458 /wd4996 /nologo
-rem set INCLUDES=/I SDL2\include
-rem set LIBS=SDL2\lib\x64\SDL2.lib SDL2\lib\x64\SDL2main.lib Shell32.lib
 
-cl.exe %CFLAGS ./src/main.c ./src/eight_utils.c ./src/headers/eight_utils
+cl.exe %CFLAGS /Fe"c_doodles.exe" /src/main.c /src/eight_utils.c /src/headers/eight_utils.h ^
+  -SUBSYSTEM:windows
