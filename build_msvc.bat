6a88479a796b982a52ad28920650be27face0c88
octopusnz <23374251+octopusnz@users.noreply.github.com>
2020-12-07 11:37:09 +1300

    Update build_msvc.bat

--- build_msvc.bat
+++ build_msvc.bat
@@ -3,5 +3,5 @@ rem launch this from msvs-enabled console
 
 set CFLAGS=/std:c17 /O2 /FC /Zi /W4 /WX /wd4458 /wd4996 /nologo
 
-cl.exe %CFLAGS /Fe"c_doodles.exe" /src/main.c /src/eight_utils.c /src/headers/eight_utils.h ^
+cl.exe %CFLAGS% /Fe"c_doodles.exe" /src/main.c /src/eight_utils.c /src/headers/eight_utils.h ^
   -SUBSYSTEM:windows
